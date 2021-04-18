2928cf5c302532c0bfaf641405f86bc74459fa81
Yongbin Wei <yongbin@momo.local>
2021-04-18 22:31:36 +0200

    Update install.m

--- install.m
+++ install.m
@@ -1,6 +1,8 @@
 % Run this script for the first time to retrieve necessary data 
 clc, clear, close all
 filepath = fileparts(mfilename('fullpath'));
+addpath(genpath(filepath));
+savepath;
 
 if ~exist(fullfile(filepath, 'output'), 'dir')
     mkdir(fullfile(filepath, 'output'));
@@ -30,11 +32,10 @@ unzip(outfilename, fullfile(filepath, 'src'));
 delete(outfilename);
 fprintf('%s\n', 'finished');
 
-% Download spin data
-fprintf('%s', '## Download expression data for the null-spin model (~1.5G) ...');
+% Download data for the null-spatial model
+fprintf('%s', '## Download expression data for the null-spatial model (~1.5G) ...');
 outfilename = websave(fullfile(filepath, 'src', 'gene_expression_spin.zip'), ...
     'https://www.dropbox.com/s/nwmtqro3dkma3u1/gene_expression_spin.zip?dl=1');
 unzip(outfilename, fullfile(filepath, 'src'));
 delete(outfilename);
 fprintf('%s\n', 'finished');
-
