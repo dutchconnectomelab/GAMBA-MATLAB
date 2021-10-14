8094355eafe5f613cbddc401c1cc498a20c4a2a8
yongbin-wei <robin-way@hotmail.com>
2021-10-14 15:57:51 +0200

    Updated null-brain-gene model (expression)

--- src/permutation_expression_null_brain.m
+++ src/permutation_expression_null_brain.m
@@ -1,4 +1,4 @@
-function res = permutation_expression_null_brain(geneset, expressions, gene_symbols)
+function res = permutation_expression_null_brain(geneset, expressions, gene_symbols, background)
 % PERMUTATION_EXPRESSION_NULL_BRAIN(GENESET, EXPRESSIONS, GENE_SYMBOLS) 
 % performs permutation testing to examine in which regions the input gene 
 % set is overexpressed.
@@ -13,6 +13,13 @@ function res = permutation_expression_null_brain(geneset, expressions, gene_symb
 %   gene_symbols -- a cell array of gene symbols of all genes. This must be
 %       provided if EXPRESSIONS is provided. If not available, gene symbols
 %       will be loaded from the default gene expression data.
+%   background -- a string indicating the selection of brackground genes
+%       Options: "brain" -- genes over-expressed in brain compared to other 
+%                body sites (N=2655), default
+%                "body" -- genes over-/similarly expressed in brain tissue
+%                compared to other body sites (N=8296)
+%                "general" -- genes expressed in brain without contrast to
+%                other body sites (N=16778)
 %
 % OUTPUT
 %   res.p -- two-tailed p-value in permutation testing
@@ -28,16 +35,31 @@ function res = permutation_expression_null_brain(geneset, expressions, gene_symb
 
 % ========================== Check input data =============================
 disp('Runing null-brain model');
+filepath = fileparts(mfilename('fullpath'));
 
 if nargin == 1
-    filepath = fileparts(mfilename('fullpath'));
     data_ge = load(fullfile(filepath, 'gene_expression.mat'));
     disp('## Loading default gene expression data in DK114 atlas ...');
     expressions = data_ge.mDataGEctx;
     gene_symbols = data_ge.gene_symbols;
     regionDescriptions = data_ge.regionDescriptionCtx;
+    background = 'brain';
 elseif nargin == 2
     error('Please provide gene symbols of all genes included in the expression data.');
+elseif nargin == 3
+    background = 'brain';
+end
+
+if isempty(expressions)
+    warning('"expressions" is empty. Loading default expression matrix.');
+    data_ge = load(fullfile(filepath, 'gene_expression.mat'));
+    expressions = data_ge.mDataGEctx;
+end
+
+if isempty(gene_symbols)
+    warning('"gene_symbols" is empty. Loading default expression matrix.');
+    data_ge = load(fullfile(filepath, 'gene_expression.mat'));
+    gene_symbols = data_ge.gene_symbols;
 end
 
 [N, K] = size(expressions);
@@ -59,10 +81,24 @@ end
 disp(['## ', num2str(nnz(II)), '/', num2str(NG), ' genes with gene expression data available.']);
 
 % load brain genes
-filepath = fileparts(mfilename('fullpath'));
-data_ge = load(fullfile(filepath, 'gene_expression.mat'), 'BRAINgene_idx', 'gene_symbols');
-BRAINgenes = data_ge.gene_symbols(data_ge.BRAINgene_idx);
+data_ge = load(fullfile(filepath, 'gene_expression.mat'), 'BRAINgene_idx',...
+    'BRAINandBODYgene_idx', 'BRAIN_expressed_gene_idx', 'gene_symbols');
 
+switch background
+    case 'brain'
+        BRAINgenes = data_ge.gene_symbols(data_ge.BRAINgene_idx);
+        disp(['## Background is selected as genes over-expressed in brain, N=',num2str(numel(BRAINgenes))]);
+    case 'body'
+        BRAINgenes = data_ge.gene_symbols(data_ge.BRAINandBODYgene_idx);
+        disp(['## Background is selected as genes over- or similarly expressed in brain compared to other body sites, N=',num2str(numel(BRAINgenes))]);
+    case 'general'
+        BRAINgenes = data_ge.gene_symbols(data_ge.BRAIN_expressed_gene_idx);
+        disp(['## Background is selected as genes expressed in brain, without contrast to other body sites, N=',num2str(numel(BRAINgenes))]);
+    otherwise
+        warning('Background genes are not properly selected. Setting to "brain" by default.')
+        BRAINgenes = data_ge.gene_symbols(data_ge.BRAINgene_idx);
+        disp(['## Background is selected as genes over-expressed in brain, N=',num2str(numel(BRAINgenes))]);
+end
 geneset = intersect(geneset, BRAINgenes);
 II = ismember(gene_symbols, geneset);
 disp(['## ', num2str(nnz(II)), ' genes of the input GOI are brain-enriched genes.']);
